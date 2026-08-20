586e6444a7deac5c46685db24d88db94d49e9660
Xavier Gomez <112890886+redgumer@users.noreply.github.com>
2026-08-20 16:42:07 +0200

    Use public Turnstile site key directly

--- functions/api/public-config.js
+++ functions/api/public-config.js
@@ -1,6 +1,6 @@
-export async function onRequestGet({ env }) {
+export async function onRequestGet() {
   return new Response(JSON.stringify({
-    turnstileSiteKey: env.TURNSTILE_SITE_KEY || ''
+    turnstileSiteKey: '0x4AAAAAAAEWf1fOAxL2xtgbCS'
   }), {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
