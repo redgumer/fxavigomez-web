b29daf296a38c1c19b9309f84db8e1866aaf7cc5
Xavier Gomez <112890886+redgumer@users.noreply.github.com>
2026-08-21 18:43:44 +0200

    Keep public Turnstile key in sync

--- functions/api/public-config.js
+++ functions/api/public-config.js
@@ -1,6 +1,6 @@
 export async function onRequestGet() {
   return new Response(JSON.stringify({
-    turnstileSiteKey: '0x4AAAAAAAEWffOAxL2xtgbCS'
+    turnstileSiteKey: '0x4AAAAAAEWffOAxL2xtgbCS'
   }), {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
