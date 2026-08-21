ce92f0460edea1339642ace6f1a8c81e4c423af1
Xavier Gomez <112890886+redgumer@users.noreply.github.com>
2026-08-21 08:54:30 +0200

    Fix Turnstile site key

--- functions/api/public-config.js
+++ functions/api/public-config.js
@@ -1,6 +1,6 @@
 export async function onRequestGet() {
   return new Response(JSON.stringify({
-    turnstileSiteKey: '0x4AAAAAAAEWf1fOAxL2xtgbCS'
+    turnstileSiteKey: '0x4AAAAAAAEWffOAxL2xtgbCS'
   }), {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
